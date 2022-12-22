90330f4e4761ffdd14cfab6ec7c3dea20f076786
Daira <dairabezzato@gmail.com>
2022-12-22 13:23:24 -0300

    updated apicalls scripts and notes

--- Documentacion/BBDD/Steps/script for testing bbdd.sql
+++ Documentacion/BBDD/Steps/script for testing bbdd.sql
@@ -3,13 +3,14 @@
 ########################################
 #call create_user('pedrito', 'unacontrasenia','play');
 #call create_user('juancito', 'unacontrasenia','play');
-
 #call create_user('holisholis', 'unamejorpass','play');
 #call create_user_information(18,'ajore', 'bububu','35468974','1998-5-12', 'unemailrelindo@gmail.com');
 #call create_user('pedritotrx', 'altapasspedrito','tree');
+#call create_user('terere', 'rererere','tukulu');
+#call create_user_information(22,'teresa', 'lupus','25658457','1998-8-12', 'teresital@gmail.com');
 #call get_all_users();
 #call create_user_information(20,'dsfgaf', 'fasdf','454566825','1998-8-25', '12dssafds@gmail.com');
-call get_all_users();
+#call get_all_users();
 #call get_all_users_information();
 #call delete_user(6)
 #call get_user_information(16)
@@ -34,6 +35,13 @@ call get_all_users();
 #call create_group('preceptores', 'preceptores del instituto');
 #call delete_group(1)
 #call get_all_groups()
+#call get_users_for_group(3);
+#call add_user_to_group(3,16);
+#call add_user_to_group(3,17);
+#call add_user_to_group(4,18);
+#call add_user_to_group(4,19);
+#call add_user_to_group(3,20);
+#call add_user_to_group(3,22);
 #########################################
 #SUBJECT#
 ########################################
@@ -64,11 +72,6 @@ call get_all_users();
 #call create_action('cargar materias', 'usuario puede cargar materias')
 #call get_all_actions()
 #######################################
-#call get_users_for_group(4)
-#call add_user_to_group(3,16);
-#call add_user_to_group(3,17);
-#call add_user_to_group(4,18);
-#call add_user_to_group(4,19);
 #######################################
 #call create_period('primer cuatrimestre', '2022-03-12', '2022-07-12');
 #call create_period('segundo cuatrimestre', '2022-07-12', '2022-11-12');
@@ -81,9 +84,9 @@ call get_all_users();
 #call create_student_subject(1, 17, 2);
 #call create_student_subject(2, 17, 3);
 #call create_student_subject(3, 17, 4);
-
 #call get_students_for_subject(2);
 #call get_attendance_information_for_subject(2);
 #call create_attendance(2, 16, '2022-03-18', true );
 #call create_attendance(2, 16, '2022-03-25', false );
-#call create_attendance(2, 16, '2022-04-15', true );attendanceattendance
+#call create_attendance(2, 16, '2022-04-15', true );
+#call get_attendance_information_for_subject(2);
